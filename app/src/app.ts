13750dbbf6d1634280b7ef562f0e9c8ecc034242
Vitalii Mishchenko <vitalii.mishchenko@mongodb.com>
2023-10-03 22:14:21 -0400

    Schedule transcription with ADMIN_ACCESS_TOKEN

--- app/src/app.ts
+++ app/src/app.ts
@@ -17,11 +17,15 @@ const artifactRepository = new ArtifactRepositoryApp(
 const app = express()
 
 app.get('/', async function (request, response) {
+    response.send(`Use /api/audio to get transcription, e.g. /api/audio/?url=https://eslyes.com/easydialogs/audio/dailylife010.mp3`);
+});
+
+app.post('/api/audio', async function (request, response) {
     // validate request
-    const audioUrl = request.query['audioUrl'] as string;
+    const audioUrl = request.query['url'] as string;
     if (!audioUrl) {
         response.statusCode = 400;
-        response.end(`Specify "audioUrl" query parameter, e.g. /?audioUrl=https://eslyes.com/easydialogs/audio/dailylife010.mp3`);
+        response.end(`Specify "url" query parameter, e.g. /api/audio/?url=https://eslyes.com/easydialogs/audio/dailylife010.mp3`);
         return;
     }
 
@@ -37,14 +41,50 @@ app.get('/', async function (request, response) {
 
     // delete stale artifact
     if (isStale) {
+        console.log(`Delete stale artifact, artifactId: ${existingArtifact._id!}`);
         await artifactRepository.deleteArtifact(existingArtifact._id!);
-        console.log(`Deleted stale artifact, artifactId: ${existingArtifact._id!}`);
+
+        const newArtifact = await artifactRepository.createArtifact(audioUrl);
+        console.log(`Re-scheduled to process stale artifact, new artifactId: ${newArtifact._id}, audioUrl, ${audioUrl}.`);
+
+        response.send(mapArtifactToView(newArtifact));
+        return;
     }
 
-    // schedule audio processing
-    const newArtifact = await artifactRepository.createArtifact(audioUrl);
-    console.log(`Scheduled a new task to process audio, artifactId: ${newArtifact._id}, audioUrl, ${audioUrl}.`);
-    response.send(mapArtifactToView(newArtifact));
+    // Artifact doesn't exist at this point. Schedule processing if ADMIN_ACCESS_TOKEN is valid or absent.
+    const REQUEST_ADMIN_ACCESS_TOKEN = request.body.ADMIN_ACCESS_TOKEN;
+    if (!config.ADMIN_ACCESS_TOKEN || config.ADMIN_ACCESS_TOKEN === REQUEST_ADMIN_ACCESS_TOKEN) {
+        const newArtifact = await artifactRepository.createArtifact(audioUrl);
+        console.log(`Scheduled to process a new audio, artifactId: ${newArtifact._id}, audioUrl, ${audioUrl}.`);
+
+        response.send(mapArtifactToView(newArtifact));
+        return;
+    }
+
+    // No artifact for the audio. Also, request doesn't have a valid ADMIN_ACCESS_TOKEN to schedule processing.
+    response.statusCode = 404;
+    response.end(`No transcription for requested audio. Provide "ADMIN_ACCESS_TOKEN" to schedule transcription.`);
+});
+
+app.get('/api/audio', async (request, response) => {
+    // validate request
+    const audioUrl = request.query['url'] as string;
+    if (!audioUrl) {
+        response.statusCode = 400;
+        response.end(`Specify "url" query parameter, e.g. /api/audio/?url=https://eslyes.com/easydialogs/audio/dailylife010.mp3`);
+        return;
+    }
+
+    // find existing artifact
+    const existingArtifact = await artifactRepository.findArtifact(audioUrl);
+
+    if (!existingArtifact){
+        response.statusCode = 404;
+        response.end(`No transcription for requested audio. Use POST request to schedule transcription.`);
+        return;
+    }
+
+    response.send(mapArtifactToView(existingArtifact));
 });
 
 const port = 3000;
